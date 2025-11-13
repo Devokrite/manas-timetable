3ce483322dc0d360e98a7d10a43207440837cc90
Devokrite <75541047+Devokrite@users.noreply.github.com>
2025-11-13 15:37:12 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "info.manas.edu.kg",
+      },
+    ],
   /* config options here */
   reactCompiler: true,
 };
+}
 
 export default nextConfig;
