ee72455285d40584062d777765e1ef6bdecb67b7
Devokrite <75541047+Devokrite@users.noreply.github.com>
2025-11-16 00:07:01 +0600

    Update google-translate-api.d.ts

--- src/types/google-translate-api.d.ts
+++ src/types/google-translate-api.d.ts
@@ -1,1 +1,12 @@
+declare module '@vitalets/google-translate-api' {
+  interface TranslationResult {
+    text: string;
+    from: { language: { iso: string } };
+    raw: string;
+  }
 
+  export default function translate(
+    text: string,
+    options: { from?: string; to: string }
+  ): Promise<TranslationResult>;
+}
