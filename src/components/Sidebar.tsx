de953eb00631463908ae6a3dbaa4efa1209a8b4f
Devokrite <75541047+Devokrite@users.noreply.github.com>
2026-01-11 17:57:22 +0600

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { CalendarDays } from "lucide-react";
+import { Map } from "lucide-react";
+
 
 
 // --- ICONS ---
